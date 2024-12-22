a190841b9fe9a16754ed520d330a747b214b8973
Aisenesia <keser46h3@gmail.com>
2024-12-22 21:44:26 +0300

    final, delete dups, broken cycles and false passthrough

--- hw4/pro.lisp
+++ hw4/pro.lisp
@@ -1,79 +1,91 @@
-(defun return-matching-rules (query axioms &optional list)
-  "Find matching rules for goal in axioms"
-  (let ((ret-list (if (null list) '() list)))
-    (dolist (axiom axioms)
-      (progn
-        (format t "DEBUG: query: ~A Matching axiom: ~A, list: ~A~%" query axiom ret-list)
-        (cond
-          ;; Case 1: Direct predicate match (unchanged)
-          ((equal (car query) (car axiom))
-           (progn
-             (format t "DEBUG: Direct match: ~A~%" axiom)
-             (cond
-               ((and (not (variable-p (cadr axiom)))
-                    (not (variable-p (cadr query))))
-               (when (equal (cadr axiom) (cadr query))
-                 (push axiom ret-list)))
-               ((or (variable-p (cadr axiom))
-                    (variable-p (cadr query)))
-                (let ((match-found t))
-                  (when (and (not (variable-p (caddr axiom)))
-                           (not (variable-p (caddr query)))
-                           (not (equal (caddr axiom) (caddr query))))
-                    (setf match-found nil))
-                  (when match-found
-                    (push axiom ret-list)))))))
-         
-          ;; Case 2: Rule match with "<" - transform query and recurse
-          ((and (listp axiom)
-                (listp (car axiom))
-                (equal (caar axiom) (car query))
-                (member "<" (cdr axiom) :test #'equal))
-           (progn
-             (format t "DEBUG: Found rule with head: ~A and body: ~A~%" (car axiom) (cddr axiom))
-             ;; For each body predicate, create a new query by substituting variables
-             (dolist (body-pred (cddr axiom))
-               (let* ((head-vars (cdar axiom))  ; Variables in the head
-                      (query-vals (cdr query))   ; Values in the query
-                      ;; Create new query by replacing variables in body predicate
-                      (new-query (substitute-vars body-pred head-vars query-vals)))
-                 (format t "DEBUG: Created new query: ~A~%" new-query)
-                 (let ((sub-matches (return-matching-rules new-query axioms)))
-                   (setf ret-list (append sub-matches ret-list))))))))))
-    ret-list))
+(defparameter *max-depth* 5 "Maximum recursion depth")
+;; The depth limit is used to prevent infinite recursion and cycles in complex queries,
+;; cycle detection is kind of broken so..
 
 (defun substitute-vars (predicate head-vars query-vals)
   "Substitute variables in predicate with values from query"
-  (if (listp predicate)
-      (mapcar #'(lambda (term)
-                  (let ((pos (position term head-vars :test #'equal)))
+  (if (listp predicate) ; If predicate is a list, map over it
+      (mapcar #'(lambda (term) ; For each term in the predicate
+                  (let ((pos (position term head-vars :test #'equal))) ; Find the position of the term in the head-vars
                     (if pos
-                        (nth pos query-vals)
-                        term)))
+                        (nth pos query-vals) ; If the term is a variable, 
+                        ;                      substitute it with the corresponding value from query-vals
+                        term))) ; Otherwise, return the term as is
               predicate)
       predicate))
     
 (defun variable-p (term)
   "Check if a symbol is a variable (starts with uppercase)"
-  (and (stringp term)
+  (and (stringp term) ; Check if term is a string, and the first character is an uppercase letter
        (alpha-char-p (char term 0))
        (upper-case-p (char term 0))))
 
-;; Test the implementation
-(defun testspr ()
+(defun prolog_prove (query axioms &optional (depth 0) (visited nil))
+  "Find matching rules for goal in axioms with depth limit"
+  ; (format t "~%DEBUG: Processing query ~A at depth ~A" query depth) ; Debug output
+  
+  (when (>= depth *max-depth*)
+    (format t "~%WARNING: Max depth ~A reached for query ~A~%" depth query)
+    (return-from prolog_prove nil))
+  
+  (let ((ret-list '())
+        (query-key (format nil "~A-~A" (car query) (caddr query)))) ; Generate a unique key for the query to detect cycles
+    
+    (when (member query-key visited :test #'equal)
+      (format t "~%WARNING: Cycle detected for ~A~%" query-key)
+      (return-from prolog_prove nil)) ; break
+    
+    (dolist (axiom axioms) ; for each axiom in the list of axioms
+      (cond
+        ;; Direct fact match
+        ((and (not (listp (car axiom))) ; If the axiom is not a list
+              (equal (length query) (length axiom)) ; and the query and axiom have the same length
+              (every #'(lambda (q a) ; and every element in the query and axiom are equal or the query element is a variable
+                         (or (variable-p q) (equal q a)))
+                     query axiom))
+         (unless (member axiom ret-list :test #'equal)
+           (push axiom ret-list))) ; Add the axiom to the return list since we can conclude its a direct fact match
+        
+        ;; Rule match
+        ;; A rule match occurs when the head of the axiom matches the query's key. 
+        ((and (listp (car axiom)) ; If the axiom is a list
+              (equal (caar axiom) (car query))) ; and the head of the axiom matches the query's key
+         (let* ((head-vars (cdar axiom)) 
+                (query-vals (cdr query)) 
+                (body-preds (cddr axiom))) ; Extract the head variables, query values and body predicates
+            ; (format t "~%DEBUG: Matching rule ~A with query ~A" axiom query)
+           (dolist (body-pred body-preds) ; For each body predicate in the body predicates
+             (unless (equal body-pred "<") ; if not <
+               (let ((new-query (substitute-vars body-pred head-vars query-vals))) ; change the variables in the body predicate with the query values
+                 ;(format t "~%DEBUG: Generated subquery ~A" new-query)
+                 (let ((sub-matches (prolog_prove ; recursively call prolog_prove with the new query
+                                    new-query 
+                                    axioms 
+                                    (1+ depth)
+                                    (cons query-key visited)))) ; add the query key to the visited list
+                   (dolist (match sub-matches)
+                     (unless (member match ret-list :test #'equal)
+                       (push match ret-list)))))))))))
+    ; (format t "~%DEBUG: Returning matches ~A for query ~A" ret-list query)
+    ret-list)) ; return the list of matches
+
+(defun run-tests ()
   (let ((axioms '(("father" "jim" "jill")
                   ("mother" "mary" "jill")
-                  ("mother" "mary" "zoe")
                   ("father" "samm" "jim")
                   (("ancestor" "X" "Y") "<" ("parent" "X" "Y"))
                   (("ancestor" "X" "Y") "<" ("ancestor" "X" "Z") ("ancestor" "Z" "Y"))
                   (("parent" "X" "Y") "<" ("mother" "X" "Y"))
-                  (("parent" "X" "Y") "<" ("father" "X" "Y"))))
-        (query1 '("ancestor" "Z" "jill"))
-        (query2 '("parent" "X" "jill"))
-        (query3 '("ancestor" "X" "zoe")))
-    (format t "~%Testing father query: ~A~%" (return-matching-rules query1 axioms))))
-    ;(format t "~%Testing parent query: ~A~%" (return-matching-rules query2 axioms))
-    ;(format t "~%Testing ancestor query: ~A~%" (return-matching-rules query3 axioms))))
+                  (("parent" "X" "Y") "<" ("father" "X" "Y")))))
+    
+    (format t "~%Test 1 - Direct fact query:")
+    (print (prolog_prove '("father" "jim" "X") axioms))
+    
+    (format t "~%Test 2 - Parent rule query:")
+    (print (prolog_prove '("parent" "X" "jill") axioms))
+      
+    
+    (format t "~%Test 3 - Ancestor query with depth limit:")
+    (print (prolog_prove '("ancestor" "X" "jill") axioms))))
 
-(testspr)
+(run-tests)
